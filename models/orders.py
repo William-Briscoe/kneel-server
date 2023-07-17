bb83522c61a28ee3d3ffcd600e7a7574b5a3842d
William Briscoe <will.m.briscoe@gmail.com>
2023-07-17 14:04:27 -0500

    implemented custom class modules and initialize functions for each

---
+++ models/orders.py
@@ -0,0 +1,7 @@
+class Order():
+    """Class that defines the properties for an order object"""
+    def __init__(self, id, metalId, styleId, sizeId):
+        self.id = id
+        self.metalId = metalId
+        self.styleId = styleId
+        self.sizeId = sizeId
---
+++ models/sizes.py
@@ -0,0 +1,6 @@
+class Size():
+    """Class that defines the properties for a size object"""
+    def __init__(self, id, carets, price):
+        self.id = id
+        self.carets = carets
+        self.price = price
